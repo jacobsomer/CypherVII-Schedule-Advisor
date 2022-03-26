11034917b8034010dc1cc7224922340e2e23622c
Joseph S. Lee <jslee08@email.wm.edu>
2022-03-26 19:57:30 -0400

    Prettify App and Login components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,29 +4,23 @@ import Questions from "./components/Questions.js";
 import Login from "./components/Login.js";
 
 export function Render() {
-    const [validAddress, setValidAddress] = useState("");
-    switch (validAddress) {
-        case "valid":
-            console.log("hello4");
-            return <Questions />;
-        case "invalid":
-            console.log("hello3");
-            return <></>; //<InvalidEmail/>;
-        case "notYetServed":
-            console.log("hello2");
-            return <></>; //<NotYetServed />;
-        default:
-            console.log("hello1");
-            return (
-                <Login
-                    validAddress={validAddress}
-                    setValidAddress={setValidAddress}
-                />
-            );
-    }
+  const [validAddress, setValidAddress] = useState("");
+  switch (validAddress) {
+    case "valid":
+      return <Questions />;
+    case "invalid":
+      return <></>; //<InvalidEmail/>;
+    case "notYetServed":
+      return <></>; //<NotYetServed />;
+    default:
+      console.log("hello1");
+      return (
+        <Login validAddress={validAddress} setValidAddress={setValidAddress} />
+      );
+  }
 }
 function App() {
-    return <div className="background">{Render()}</div>;
+  return <div className="background">{Render()}</div>;
 }
 
 export default App;
