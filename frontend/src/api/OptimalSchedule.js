907725db35328ecee129e9609dfdf29b4a035895
jacobsomer <jsomer@email.wm.edu>
2022-03-26 21:44:31 -0700

    fix merge conflicts

--- frontend/src/api/OptimalSchedule.js
+++ frontend/src/api/OptimalSchedule.js
@@ -1,15 +1,13 @@
 import { react } from "react";
 
 async function OptimalSchedule(scheduleParams) {
-  const major = scheduleParams.major;
-  const response = await fetch(
-    "http://localhost:8080/getOptimalSchedule/" + major
-  );
+    const major = scheduleParams.major;
 
-  // const json = await response;
-  const json = await response.json();
-  console.log(json);
-  return json;
+    const response = await fetch(
+        "http://localhost:8080/getOptimalSchedule/" + major,
+    );
+    const json = await response.json();
+    return json;
 }
 
 export default OptimalSchedule;
