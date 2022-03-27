29ef163b41e6bece1a35b7fc54d37009034a3f23
Joseph S. Lee <jslee08@email.wm.edu>
2022-03-26 23:03:42 -0400

    Add button to generate new schedule

--- frontend/src/api/OptimalSchedule.js
+++ frontend/src/api/OptimalSchedule.js
@@ -1,18 +1,18 @@
 import { react } from "react";
 
 async function OptimalSchedule(props) {
-    const params = {
-        major1: props.major1,
-        major2: props.major2,
-        minor1: props.minor1,
-        minor2: props.minor2,
-    };
-    const response = await fetch(
-        "http://localhost:8080/getOptimalSchedule/" + JSON.stringify(params),
-    );
-    const json = await response.json();
-    console.log("json");
-    return json;
+  const params = {
+    major1: props.major1,
+    major2: props.major2,
+    minor1: props.minor1,
+    minor2: props.minor2,
+  };
+  const response = await fetch(
+    "http://localhost:8080/getOptimalSchedule/" + JSON.stringify(params)
+  );
+  const json = await response.json();
+  console.log("json");
+  return json;
 }
 
 export default OptimalSchedule;
--- frontend/src/components/Planner/Planner.js
+++ frontend/src/components/Planner/Planner.js
@@ -1,101 +1,118 @@
-import React from "react";
+import { React, useState } from "react";
 import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import OptimalSchedule from "../../api/OptimalSchedule";
 
 const useStyles = makeStyles((theme) =>
-    createStyles({
-        Semester: {
-            // paddingTop: "-10px",
-            padding: theme.spacing(2),
-            textAlign: "center",
-            color: theme.palette.text.secondary,
-        },
-        root: {
-            flexGrow: 1,
-        },
-    }),
+  createStyles({
+    Semester: {
+      // paddingTop: "-10px",
+      padding: theme.spacing(2),
+      textAlign: "center",
+      color: theme.palette.text.secondary,
+    },
+    root: {
+      flexGrow: 1,
+    },
+  })
 );
 
 export default function Planner() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div
+  const [major, setMajor] = useState("");
+  const [schedule, setSchedule] = useState("");
+
+  function handleGenerateClick() {
+    // submit params used to generate possible schedule
+    const scheduleParams = {
+      major: major,
+      // TODO: add previous courses taken to params
+    };
+    setSchedule(OptimalSchedule(scheduleParams));
+  }
+
+  return (
+    <div
+      style={{
+        position: "absolute",
+        width: "90%",
+        backgroundColor: "rgba(0,0,0,.7)",
+        padding: "10px",
+        transform: "translate(-50%)",
+        left: "50%",
+        borderRadius: "10px",
+        height: "fitContent",
+        blockSize: "fitContent",
+      }}
+    >
+      <div
+        style={{
+          position: "relative",
+          width: "90%",
+          backgroundColor: "transparent",
+          padding: "10px",
+          transform: "translate(-50%)",
+          left: "50%",
+          borderRadius: "10px",
+        }}
+      >
+        <Button variant="contained" onClick={() => handleGenerateClick()}>
+          Generate New Schedule
+        </Button>
+        <Grid container spacing={3}>
+          <div
             style={{
-                position: "absolute",
-                width: "90%",
-                backgroundColor: "rgba(0,0,0,.7)",
-                padding: "10px",
-                transform: "translate(-50%)",
-                left: "50%",
-                borderRadius: "10px",
-                height: "fitContent",
-                blockSize: "fitContent",
+              position: "relative",
+              width: "80%",
+              backgroundColor: "white",
+              height: "23px",
+              padding: "40px",
+              transform: "translate(-50%)",
+              left: "50%",
+              top: "20px",
+              borderRadius: "5px",
             }}
-        >
-            <div
-                style={{
-                    position: "relative",
-                    width: "90%",
-                    backgroundColor: "transparent",
-                    padding: "10px",
-                    transform: "translate(-50%)",
-                    left: "50%",
-                    borderRadius: "10px",
-                }}
-            >
-                <Grid container spacing={3}>
-                    <div
-                        style={{
-                            position: "relative",
-                            width: "80%",
-                            backgroundColor: "white",
-                            height: "23px",
-                            padding: "40px",
-                            transform: "translate(-50%)",
-                            left: "50%",
-                            top: "20px",
-                            borderRadius: "5px",
-                        }}
-                    >
-                        <h2>Schedule</h2>
-                    </div>
-                    <div
-                        style={{
-                            position: "relative",
-                            width: "80%",
-                            backgroundColor: "transparent",
-                            height: "0px",
-                            padding: "20px",
-                        }}
-                    ></div>
-                    <Grid item xs={12} sm={6}>
-                        <Paper className={classes.Semester}>Fall 2022</Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <Paper className={classes.Semester}>Spring 2023</Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <Paper className={classes.Semester}>Fall 2023</Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <Paper className={classes.Semester}>Spring 2024</Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <Paper className={classes.Semester}>Fall 2024</Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <Paper className={classes.Semester}>Spring 2025</Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <Paper className={classes.Semester}>Fall 2025</Paper>
-                    </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <Paper className={classes.Semester}>Spring 2026</Paper>
-                    </Grid>
-                </Grid>
-            </div>
-        </div>
-    );
+          >
+            <h2>Schedule</h2>
+          </div>
+          <div
+            style={{
+              position: "relative",
+              width: "80%",
+              backgroundColor: "transparent",
+              height: "0px",
+              padding: "20px",
+            }}
+          ></div>
+          <Grid item xs={12} sm={6}>
+            <Paper className={classes.Semester}>Fall 2022</Paper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper className={classes.Semester}>Spring 2023</Paper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper className={classes.Semester}>Fall 2023</Paper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper className={classes.Semester}>Spring 2024</Paper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper className={classes.Semester}>Fall 2024</Paper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper className={classes.Semester}>Spring 2025</Paper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper className={classes.Semester}>Fall 2025</Paper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper className={classes.Semester}>Spring 2026</Paper>
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+  );
 }
