a68d8a3beb5efcd336387dc22bf0a9075b1cb407
jacobsomer <jsomer@email.wm.edu>
2022-03-26 21:42:36 -0700

    vendor file update

--- frontend/src/api/OptimalSchedule.js
+++ frontend/src/api/OptimalSchedule.js
@@ -1,18 +1,17 @@
 import { react } from "react";
 
 async function OptimalSchedule(props) {
-  const params = {
-    major1: props.major1,
-    major2: props.major2,
-    minor1: props.minor1,
-    minor2: props.minor2,
-  };
-  const response = await fetch(
-    "http://localhost:8080/getOptimalSchedule/" + JSON.stringify(params)
-  );
-  const json = await response.json();
-  console.log("json");
-  return json;
+    const ma = {
+        major1: props.major1,
+        major2: props.major2,
+        minor1: props.minor1,
+        minor2: props.minor2,
+    };
+    const response = await fetch(
+        "http://localhost:8080/getOptimalSchedule/" + major,
+    );
+    const json = await response.json();
+    return json;
 }
 
 export default OptimalSchedule;
